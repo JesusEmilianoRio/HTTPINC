da1566b768dbcbf7ff063ae5bce6328089fa11ba
JesusEmilianoRio <jesus.emiliano123@hotmail.com>
2026-06-13 11:27:36 -0600

    Create a hash function.

--- request/requestHeader/requestHeader.c
+++ request/requestHeader/requestHeader.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stdio.h>
 #define FNV_OFFSET 14695981039346656037UL
 #define FNV_PRIME 1099511628211UL
 
@@ -6,7 +8,20 @@ typedef struct _hash {
 	char* value;
 } Hash;
 
+static uint64_t hash_key(const char *key) {
+	uint64_t hash = FNV_OFFSET;
+
+	for (const char *ptr = key; *ptr; ptr++) {
+		hash ^= (uint64_t)ptr;
+		hash *= FNV_PRIME;
+	}
+
+	return hash;
+}
 
 int parseRequestHeader() {
+	uint64_t h = hash_key("emi");
+
+	printf("%zu\n", h);
 	return 0;
 }
